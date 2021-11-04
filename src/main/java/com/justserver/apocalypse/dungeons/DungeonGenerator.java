fcefd7e010d7a9a4c80a5b3fa46ab943e24c94e2
Sasha Semenishchev <sashasemenishchev@gmail.com>
2021-11-04 18:33:03 +0300

    fixed conflist

--- src/main/java/com/justserver/apocalypse/dungeons/DungeonGenerator.java
+++ src/main/java/com/justserver/apocalypse/dungeons/DungeonGenerator.java
@@ -4,7 +4,6 @@ import com.justserver.apocalypse.Apocalypse;
 
 import java.io.File;
 
-// TODO: make it working
 public class DungeonGenerator {
     public static final File FIRST_ROOM = new File(Apocalypse.getInstance().getDataFolder().getAbsolutePath() + File.separator + "maps" + File.separator + "first.room");
 }
