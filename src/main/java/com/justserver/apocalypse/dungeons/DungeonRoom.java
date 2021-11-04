b8c21263b97de2fe38a22dad0946024647fdd0de
SashaSemenishchev <sashasemenishchev@gmail.com>
2021-11-04 17:06:25 +0200

    Parent room impl

--- src/main/java/com/justserver/apocalypse/dungeons/DungeonRoom.java
+++ src/main/java/com/justserver/apocalypse/dungeons/DungeonRoom.java
@@ -3,6 +3,7 @@ package com.justserver.apocalypse.dungeons;
 
 import com.sk89q.worldedit.WorldEditException;
 import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
 
 import java.io.IOException;
 
@@ -10,12 +11,22 @@ public class DungeonRoom {
     private final RoomType roomType;
     private final int minX;
     private final int minY;
+    private final Dungeon dungeon;
 
     public DungeonRoom(Dungeon dungeon, RoomType roomType, int minX, int minY, int minZ) throws IOException, WorldEditException {
         this.roomType = roomType;
         this.minX = minX;
         this.minY = minY;
-        roomType.paste(new Location(dungeon.getWorld(), minX, minY, minZ));
+        this.dungeon = dungeon;
+        roomType.paste(new Location(dungeon.getWorld(), minX, minY, minZ), BlockFace.NORTH);
+    }
+
+    public DungeonRoom(DungeonRoom parent, RoomType roomType, int minX, int minY, int minZ) throws IOException, WorldEditException {
+        this.roomType = roomType;
+        this.dungeon = parent.getDungeon();
+        this.minX = minX;
+        this.minY = minY;
+        roomType.paste(new Location(dungeon.getWorld(), minX, minY, minZ), parent.randomDoorwayBlockface());
     }
 
     public RoomType getRoomType() {
@@ -28,5 +39,12 @@ public class DungeonRoom {
 
     public int getMinY() {
         return minY;
+    }
+
+    public Dungeon getDungeon() {
+        return dungeon;
+    }
+    public BlockFace randomDoorwayBlockface(){
+        return BlockFace.NORTH;
     }
 }
